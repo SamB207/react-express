f141d1e91ed484a74d6a7de51d1fd214a96f3ebf
SamB207 <lovecita3@gmail.com>
2023-05-17 16:26:02 -0400

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import React from 'react'
+import LinkContainer from './components/LinkContainer'
 
 function App() {
-const [message, setMessage] = useState(null)
-useEffect(() => {
-  fetch('/api')
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data)
-  setMessage(data.message)
-})
-  .catch((err) => {console.log(err)})
-}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message ? message : 'Loading...'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LinkContainer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
